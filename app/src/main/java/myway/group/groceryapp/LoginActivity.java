036fd4f6d3261aa2472d43d3dfc1e6ce9c74df8e
Abdulloh <maxmudovabdullo97@gmail.com>
2020-08-14 22:54:55 +0500

    Initial commit

---
+++ app/src/main/java/myway/group/groceryapp/LoginActivity.java
@@ -0,0 +1,19 @@
+package myway.group.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
